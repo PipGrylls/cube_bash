73fe196b77fd774141fce3b57a382355b86f7070
Pip Grylls <pipgryllsastro@gmail.com>
2022-08-11 12:26:02 +0100

    added gamestate file

---
+++ src/game_state.c
@@ -0,0 +1,5 @@
+//
+// Created by Philip Grylls on 11/08/2022.
+//
+
+#include "game_state.h"
---
+++ src/game_state.h
@@ -0,0 +1,8 @@
+//
+// Created by Philip Grylls on 11/08/2022.
+//
+
+#ifndef CUBE_BASH_GAME_STATE_H
+#define CUBE_BASH_GAME_STATE_H
+
+#endif //CUBE_BASH_GAME_STATE_H
